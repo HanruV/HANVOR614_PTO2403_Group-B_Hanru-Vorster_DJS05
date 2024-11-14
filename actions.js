b3342730318b87bb49d63436d3015754dccd50cd
Hanru Vorster <hanru.vorster@gmail.com>
2024-11-14 10:39:40 +0200

    added the  action types

---
+++ actions.js
@@ -0,0 +1,6 @@
+// adding to the count
+export const addAction = () => ({ type: ADD });
+// decreasing form the count
+export const subtractAction = () => ({ type: SUBTRACT });
+// resetting the count
+export const resetAction = () => ({ type: RESET });
