0c0cea38493348120151cc972e00577019fb10a6
Hanru Vorster <hanru.vorster@gmail.com>
2024-11-14 11:42:24 +0200

    added some comments to actions to understand it better

--- actions.js
+++ actions.js
@@ -1,8 +1,10 @@
-// defining the actions
+// defining the actions - here we add constants to the actions because it will be easier to use than to use strings in the rest
+// of the code base
 export const ADD = "ADD";
 export const SUBTRACT = "SUBTRACT";
 export const RESET = "RESET";
 
+// Action Creators
 // adding to the count
 export const addAction = () => ({ type: ADD });
 // decreasing form the count
